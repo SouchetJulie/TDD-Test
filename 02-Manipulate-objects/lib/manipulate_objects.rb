20b09b3017b2cb5b1fd19e22b9531506994bb8f6
Julie Souchet <jsouchet@easyvista.com>
2022-08-23 16:28:59 +0200

    exercice 2 - manipulate objects

--- 02-Manipulate-objects/lib/manipulate_objects.rb
+++ 02-Manipulate-objects/lib/manipulate_objects.rb
@@ -1,1 +1,32 @@
+# frozen_string_literal: true
+
 # BONUS ONLY: Write the array_to_hash and palindrome? methods
+
+def array_to_hash(array)
+  raise 'Invalid array' unless (array.is_a? Array) && (array.all? { |el| el.is_a? String })
+
+  res = {}
+  array.each_with_index { |el, i| res[:"key#{i}"] = el }
+
+  res
+end
+
+def palindrome?(text)
+  raise 'Invalid string' unless text.is_a? String
+
+  is_palindrome = true
+  middle = (text.length / 2).floor
+
+  (0..middle).each do |index|
+    letter = text[index]
+    opposite = text[text.length - 1 - index]
+
+    if letter.downcase != opposite.downcase
+      is_palindrome = false
+      break
+    end
+  end
+
+  # all letters are mirrors : ✔
+  is_palindrome
+end
--- 02-Manipulate-objects/spec/manipulate_objects_spec.rb
+++ 02-Manipulate-objects/spec/manipulate_objects_spec.rb
@@ -1,3 +1,146 @@
+# frozen_string_literal: true
+
 # TO DO: Write the test that ensure:
 # - Our array_to_hash method successfully converts an array to a hash, with the given pattern
 # - Our palindrome? method successfully returns whether the given argument is a plindrome or not
+
+require 'manipulate_objects'
+
+describe '#array_to_hash' do
+  it 'raises an error if the parameter is not an array' do
+    # number
+    begin
+      expect(array_to_hash(1)).to eq(false) # this fails the test
+    rescue StandardError
+      expect(true).to be(true) # if here, test succeeded
+    end
+    # string
+    begin
+      expect(array_to_hash('1')).to eq(false) # this fails the test
+    rescue StandardError
+      expect(true).to be(true) # if here, test succeeded
+    end
+    # boolean
+    begin
+      expect(array_to_hash(true)).to eq(false) # this fails the test
+    rescue StandardError
+      expect(true).to be(true) # if here, test succeeded
+    end
+    # hash
+    begin
+      expect(array_to_hash({ a: 'b' })).to eq(false) # this fails the test
+    rescue StandardError
+      expect(true).to be(true) # if here, test succeeded
+    end
+  end
+
+  it 'raises an error if the array contains a number' do
+    begin
+      expect(array_to_hash([1])).to eq(false) # this fails the test
+    rescue StandardError
+      expect(true).to be(true) # if here, test succeeded
+    end
+    begin
+      expect(array_to_hash([1, 'a', 'b'])).to eq(false) # this fails the test
+    rescue StandardError
+      expect(true).to be(true) # if here, test succeeded
+    end
+    begin
+      expect(array_to_hash(['a', 1, 'b'])).to eq(false) # this fails the test
+    rescue StandardError
+      expect(true).to be(true) # if here, test succeeded
+    end
+    begin
+      expect(array_to_hash(['a', 'b', 1])).to eq(false) # this fails the test
+    rescue StandardError
+      expect(true).to be(true) # if here, test succeeded
+    end
+  end
+
+  it 'raises an error if the array contains a boolean' do
+    begin
+      expect(array_to_hash([true])).to eq(false) # this fails the test
+    rescue StandardError
+      expect(true).to be(true) # if here, test succeeded
+    end
+    begin
+      expect(array_to_hash([true, 'a', 'b'])).to eq(false) # this fails the test
+    rescue StandardError
+      expect(true).to be(true) # if here, test succeeded
+    end
+    begin
+      expect(array_to_hash(['a', true, 'b'])).to eq(false) # this fails the test
+    rescue StandardError
+      expect(true).to be(true) # if here, test succeeded
+    end
+    begin
+      expect(array_to_hash(['a', 'b', true])).to eq(false) # this fails the test
+    rescue StandardError
+      expect(true).to be(true) # if here, test succeeded
+    end
+  end
+
+  it 'returns an empty hash if the array is empty' do
+    expect(array_to_hash([])).to eq({})
+  end
+
+  it 'returns a hash' do
+    expect(array_to_hash(%w[a b])).to be_a_kind_of(Hash)
+  end
+
+  it 'returns a hash whose keys are symbols' do
+    expect(array_to_hash(%w[a b]).keys).to all(be_a(Symbol))
+  end
+
+  it 'returns a hash whose values are string' do
+    values = array_to_hash(%w[a b]).values
+    expect(values).to all(be_a(String))
+  end
+
+  it 'returns a hash that has the same values as the initial array' do
+    array = %w[a b]
+    expect(array_to_hash(array).values).to eq(array)
+  end
+
+  it 'returns a hash with incrementing "key<x>" keys with <x> matching the index in the array' do
+    array = %w[a b]
+    res = array_to_hash(array)
+    keys = res.keys
+
+    array.each_with_index do |element, index|
+      key = keys[index]
+      expect(key).to eq(:"key#{index}") # key matches
+      expect(res[key]).to eq(element) # related value matches
+    end
+  end
+end
+
+describe '#palindrome?' do
+  it 'raises an error when not given a string' do
+    begin
+      expect(palindrome?(1)).to eq(1) # fails the test
+    rescue StandardError
+      expect(true).to be(true) # succeeds the test
+    end
+    begin
+      expect(palindrome?([])).to eq(1) # fails the test
+    rescue StandardError
+      expect(true).to be(true) # succeeds the test
+    end
+    begin
+      expect(palindrome?({})).to eq(1) # fails the test
+    rescue StandardError
+      expect(true).to be(true) # succeeds the test
+    end
+  end
+
+  it 'returns a boolean' do
+    expect(palindrome?('test')).to be(true).or be(false)
+  end
+
+  it 'returns true if the word is a palindrome (case insensitive)' do
+    expect(palindrome?('test')).to be(false)
+    expect(palindrome?('tsest')).to be(true)
+    expect(palindrome?('Tsest')).to be(true)
+  end
+end
