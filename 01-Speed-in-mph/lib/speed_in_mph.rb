419fe347847341c8626ec474f00ee035bd47cbc5
Julie Souchet <jsouchet@easyvista.com>
2022-08-23 14:58:06 +0200

    exercice 1 - speed_in_mph

--- 01-Speed-in-mph/lib/speed_in_mph.rb
+++ 01-Speed-in-mph/lib/speed_in_mph.rb
@@ -1,2 +1,11 @@
+# frozen_string_literal: true
+
 # TO DO: write a method that takes a float as an argument and
 # returns it converted from kph (km/h) to mph (mi/h)
+
+def speed_in_mph(num)
+  raise "Invalid number #{num}" unless num.is_a? Numeric
+
+  # 1 km/h = 0.6213711922 mi/h
+  (num * 0.6213711922).round(2)
+end
--- 01-Speed-in-mph/spec/speed_in_mph_spec.rb
+++ 01-Speed-in-mph/spec/speed_in_mph_spec.rb
@@ -1,2 +1,56 @@
+# frozen_string_literal: true
+
 # TO DO: Write the specs that ensures our speed_in_mph method
 # successfully converts speed from km/h to mi/h.
+require 'speed_in_mph'
+
+describe '#speed_in_mph' do
+  it 'raises an error if the parameter is not a number' do
+    # string
+    begin
+      expect(speed_in_mph('a')).to eq(false) # will fail the test
+    rescue StandardError
+      expect(true).to be(true) # the code must reach here to succeed the test
+    end
+    # boolean
+    begin
+      expect(speed_in_mph(true)).to eq(false) # will fail the test
+    rescue StandardError
+      expect(true).to be(true) # the code must reach here to succeed the test
+    end
+    # array
+    begin
+      expect(speed_in_mph([1])).to eq(false) # will fail the test
+    rescue StandardError
+      expect(true).to be(true) # the code must reach here to succeed the test
+    end
+    # hash
+    begin
+      expect(speed_in_mph({ a: '1' })).to eq(false) # will fail the test
+    rescue StandardError
+      expect(true).to be(true) # the code must reach here to succeed the test
+    end
+  end
+
+  it 'returns the (positive, rounded) speed converted to mph, to 2 decimals' do
+    expect(speed_in_mph(1)).to eq(0.62)
+    expect(speed_in_mph(2)).to eq(1.24)
+    expect(speed_in_mph(10)).to eq(6.21)
+  end
+
+  it 'returns the (negative, rounded) speed converted to mph, to 2 decimals' do
+    expect(speed_in_mph(-1)).to eq(-0.62)
+    expect(speed_in_mph(-2)).to eq(-1.24)
+    expect(speed_in_mph(-10)).to eq(-6.21)
+  end
+
+  it 'returns the (positive, float) speed converted to mph, to 2 decimals' do
+    expect(speed_in_mph(3.14)).to eq(1.95)
+    expect(speed_in_mph(2.5)).to eq(1.55)
+  end
+
+  it 'returns the (negative, float) speed converted to mph, to 2 decimals' do
+    expect(speed_in_mph(3.14)).to eq(1.95)
+    expect(speed_in_mph(2.5)).to eq(1.55)
+  end
+end
